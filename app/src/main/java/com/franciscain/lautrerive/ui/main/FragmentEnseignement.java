2f869c38646f106107af8d106498256b27b81137
Termshoo <amadeo.david@student.uclouvain.be>
2021-08-03 18:36:09 +0200

    Récupération de l'évangile fonctionnel (pour une première version de l'app)

--- app/src/main/java/com/franciscain/lautrerive/ui/main/FragmentEnseignement.java
+++ app/src/main/java/com/franciscain/lautrerive/ui/main/FragmentEnseignement.java
@@ -1,14 +1,29 @@
 package com.franciscain.lautrerive.ui.main;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.franciscain.lautrerive.R;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +40,8 @@ public class FragmentEnseignement extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    TextView evangile;
 
     public FragmentEnseignement() {
         // Required empty public constructor
@@ -55,6 +72,7 @@ public class FragmentEnseignement extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
@@ -62,5 +80,42 @@ public class FragmentEnseignement extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_enseignement, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+        evangile = getView().findViewById(R.id.evangile);
+
+        new RecupereEvangile().execute();
+    }
+
+    public class RecupereEvangile extends AsyncTask<Void, Void, Void>{
+
+        String textEvangile;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Date c = Calendar.getInstance().getTime();                                              // Récupération de la date du jour
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());  // Création du format de date
+            String url = "https://www.aelf.org/" + df.format(c) + "/romain/messe";
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(url).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(TAG, e.toString());
+            }
+            String textBrut = doc.text();
+            String[] premierParsing= textBrut.split("Évangile");
+            textEvangile = "Évangile" + premierParsing[premierParsing.length-1];
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            evangile.setText(textEvangile);
+        }
     }
 }
