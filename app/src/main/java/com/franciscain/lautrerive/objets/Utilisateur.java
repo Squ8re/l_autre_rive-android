b65675cb51a567f520d55ff63c67b27e94a98087
Squ8re <amadeo.david@hotmail.com>
2021-08-05 23:56:57 +0200

    Ajout d'un nouveau constructeur.

--- app/src/main/java/com/franciscain/lautrerive/objets/Utilisateur.java
+++ app/src/main/java/com/franciscain/lautrerive/objets/Utilisateur.java
@@ -3,12 +3,19 @@ package com.franciscain.lautrerive.objets;
 public class Utilisateur {
     private String nom;
     private String email;
+    private Objectif[] objectifs;
 
     public Utilisateur(){}
 
     public Utilisateur(String nom, String email) {
-        this.nom = nom;
-        this.email = email;
+        this.nom    = nom;
+        this.email  = email;
+    }
+
+    public Utilisateur(String nom, String email, Objectif[] objectifs) {
+        this.nom        = nom;
+        this.email      = email;
+        this.objectifs  = objectifs;
     }
 
     public String getNom() {
